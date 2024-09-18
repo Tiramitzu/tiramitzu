2d144ad7c614dbe6e97bc0d3dc7721b75f6c6816
Syah Warid Ghani Akram <github@tira.my.id>
2024-09-18 14:11:03 +0700

    it was in fact the wrong project url

--- headerparser/index.js
+++ headerparser/index.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/whoami", function (req, res) {
-    const ipaddress = req.ip;
+    const ipaddress = req.headers["x-forwarded-for"];
     const language = req.headers["accept-language"];
     const software = req.headers["user-agent"];
 
